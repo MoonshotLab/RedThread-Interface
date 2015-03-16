9fc95e2478027f2e9edaecd0103f53fdddce668e
Joe Longstreet <joelongstreet@gmail.com>
2015-03-16 17:34:19 -0500

    create interface for tagging twitter posts

--- public/tag-posts.js
+++ public/tag-posts.js
@@ -1,3 +1,92 @@
 $(function(){
-  console.log('tag posts');
+  var $tweets = $('#tweets');
+
+  tweets.forEach(function(tweet){
+    // ensure these properties exist
+    if(!tweet.retweet) tweet.retweet = [];
+    if(!tweet.reply) tweet.reply = [];
+    if(!tweet.favorite) tweet.favorite = [];
+
+    // is it a regular tweet or a retweet?
+    if(tweet.original.in_reply_to_status_id) tweet.type = 'retweet';
+    else tweet.type = 'tweet';
+
+    $tweets.append(templates.tweet({
+      tweet       : tweet,
+      authorities : tags.authorities,
+      pillars     : tags.pillars
+    }));
+
+    // add an event handler so the checkboxes work
+    $('#tweet-' + tweet._id).find('input').change(function(e){
+      if($(this).prop('checked')){
+        socket.emit('tag-tweet', {
+          type  : $(this).data('type'),
+          id    : $(this).data('id'),
+          val   : $(this).val()
+        });
+      }
+    });
+  });
 });
+
+
+
+
+var templates = {};
+
+templates.tweet = _.template([
+  '<li id="tweet-<%= tweet._id %>" class="list-group-item tweet <%= tweet.type %>">',
+    '<div class="row">',
+      '<div class="col-xs-5 tweet-details">',
+        '<h4 class="tweet-text">',
+          '<%= tweet.original.text %>',
+        '</h4>',
+        '<div class="stats row">',
+          '<div class="col-xs-3">',
+            '<span class="key glyphicon glyphicon-retweet"></span>',
+            '<span class="val">',
+              '<%= tweet.retweet.length %>',
+            '</span>',
+          '</div>',
+          '<div class="col-xs-3">',
+            '<span class="key glyphicon glyphicon-star"></span>',
+            '<span class="val">',
+              '<%= tweet.favorite.length %>',
+            '</span>',
+          '</div>',
+          '<div class="col-xs-3">',
+            '<span class="key glyphicon glyphicon-share-alt"></span>',
+            '<span class="val">',
+              '<%= tweet.reply.length %>',
+            '</span>',
+          '</div>',
+        '</div>',
+      '</div>',
+      '<div class="col-xs-7 actions">',
+        '<div class="row">',
+          '<div class="col-xs-6 pillars">',
+            '<% _.each(pillars, function(pillar){ %>',
+              '<div class="checkbox">',
+                '<label>',
+                  '<input data-id="<%= tweet._id %>" data-type="pillar" type="checkbox" value=<%= pillar.name %> />',
+                  '<%= pillar.human %>',
+                '</label>',
+              '</div>',
+            '<% }); %>',
+          '</div>',
+          '<div class="col-xs-6 authorities">',
+            '<% _.each(authorities, function(authority){ %>',
+              '<div class="checkbox authority">',
+                '<label>',
+                  '<input data-id="<%= tweet._id %>" data-type="authority" type="checkbox" value=<%= authority.name %> />',
+                  '<%= authority.human %>',
+                '</label>',
+              '</div>',
+            '<% }); %>',
+          '</div>',
+        '</div>',
+      '</div>',
+    '</div>',
+  '</li>'
+].join(''));
---
+++ public/tags.js
@@ -0,0 +1,42 @@
+var tags = {
+  "pillars" : [
+    {
+      "human" : "Celebrate Existing",
+      "name"  : "celebrate"
+    },
+    {
+      "human" : "Inspire More",
+      "name"  : "inspire"
+    },
+    {
+      "human" : "Discover Need",
+      "name"  : "discover"
+    },
+    {
+      "human" : "Create Original",
+      "name"  : "create"
+    }
+  ],
+  "authorities" : [
+    {
+      "human" : "Crew",
+      "name"  : "crew"
+    },
+    {
+      "human" : "Craft",
+      "name"  : "craft"
+    },
+    {
+      "human" : "Style",
+      "name"  : "style"
+    },
+    {
+      "human" : "Music",
+      "name"  : "music"
+    },
+    {
+      "human" : "Play",
+      "name"  : "play"
+    }
+  ]
+};
